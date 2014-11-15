f4fea44115fe7a371643b24d8eef08c53562047b
rodolfo <lmlirohlml@gmail.com>
2014-11-14 21:40:51 -0700

    se creo los status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = '0b7d65a611ec6280c674628e78bce16dd3bffc875f2281bf6fdc7b1c164a4d9c5f4560a0a356d761152a7c83983b541f51710ea298d22dd0ece68e07cd185aba'
